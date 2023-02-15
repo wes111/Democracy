001934420e892fd34a4090365993830994109ed3
Wes Luntsford <wesleyluntsford@gmail.com>
2023-02-14 19:08:58 -0600

    Initial Commit

---
+++ Democracy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Democracy
+//
+//  Created by Wesley Luntsford on 2/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Democracy/DemocracyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemocracyApp.swift
+//  Democracy
+//
+//  Created by Wesley Luntsford on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DemocracyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
