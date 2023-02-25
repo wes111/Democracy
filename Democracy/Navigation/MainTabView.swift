84d236e78dcbe18f9298efa73a56b4a319d3edab
Wesley Luntsford <wesluntsford11@gmail.com>
2023-02-25 10:59:43 -0600

    Fix navigation issue with MainTab view

--- Democracy/Navigation/MainTabView.swift
+++ Democracy/Navigation/MainTabView.swift
@@ -7,46 +7,50 @@
 
 import SwiftUI
 
-enum MainTabPath: Hashable {
-    case b
+enum MainTab {
+    case one, two, three, four, five
 }
 
 struct MainTabView: View {
     
-    let mainTab = "Main"
+    @State private var selectedTab: MainTab = .three
     
     var body: some View {
-        TabView(selection: Binding.constant(mainTab)) {
+        TabView(selection: $selectedTab) {
             MainCoordinator()
                 .tabItem {
                     Label("Editor", systemImage: "pencil.circle")
                     Text("Editor")
                 }
+                .tag(MainTab.one)
             
             MainCoordinator()
                 .tabItem {
                     Label("Editor", systemImage: "pencil.circle")
                     Text("Editor")
                 }
+                .tag(MainTab.two)
             
             MainCoordinator()
                 .tabItem {
                     Label("Notes", systemImage: "note.text")
                     Text("Notes")
                 }
-                .tag(mainTab)
+                .tag(MainTab.three)
             
             MainCoordinator()
                 .tabItem {
                     Label("Share", systemImage: "square.and.arrow.up")
                     Text("Share")
                 }
+                .tag(MainTab.four)
             
-            MainCoordinator()
+            SettingsCoordinator()
                 .tabItem {
                     Label("Settings", systemImage: "gearshape")
                     Text("Settings")
                 }
+                .tag(MainTab.five)
         }
         .navigationBarBackButtonHidden()
     }
