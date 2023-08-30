403cf9b676437595dd2d5ccbc86751ac44160a94
Wesley Luntsford <wesluntsford11@gmail.com>
2023-08-29 20:15:00 -0500

    Add semaphore to router.

--- Democracy/Views/Navigation/Router.swift
+++ Democracy/Views/Navigation/Router.swift
@@ -21,31 +21,44 @@ protocol RouterProtocol: ObservableObject {
 final class Router: RouterProtocol {
     
     @Published var navigationPath = NavigationPath()
+    // Ideally we would make this an actor instead of class, but that does not work with published properties.
+    // Using semaphore instead.
+    private let semaphore: DispatchSemaphore = DispatchSemaphore(value: 1)
     
     init() {}
     
     func push(_ path: any Hashable) {
+        self.semaphore.wait()
         navigationPath.append(path)
+        self.semaphore.signal()
     }
     
     func push(_ paths: [any Hashable]) {
+        self.semaphore.wait()
         paths.forEach { navigationPath.append($0) }
+        self.semaphore.signal()
     }
     
     func pop() {
+        self.semaphore.wait()
         navigationPath.removeLast()
+        self.semaphore.signal()
     }
     
     func pop(count: Int) {
+        self.semaphore.wait()
         for _ in 0..<count {
             navigationPath.removeLast()
         }
+        self.semaphore.signal()
     }
     
     func popToRoot() {
+        self.semaphore.wait()
         for _ in 0 ..< navigationPath.count {
             navigationPath.removeLast()
         }
+        self.semaphore.signal()
     }
     
 }
